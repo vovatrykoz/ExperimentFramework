1305f31105b0abb2704b671915f781d8875b8f39
vovatrykoz <vovatrykoz@gmail.com>
2024-10-16 16:35:54 +0200

    add mailbox address sharing logic

--- source/main.cpp
+++ source/main.cpp
@@ -1,16 +1,56 @@
+#include <filesystem>
 #include <iostream>
 #include <sstream>
+#include <fstream>
 #include <string>
 
 #include "queue/ThreadSafeQueue.h"
 
 int main(void) {
+    std::cout << "Starting setup..." << std::endl;
+
     ThreadSafeQueue messageQueue;
 
-    std::ostringstream oss;
-    oss << &messageQueue;
-    std::string addressStr = oss.str();
-    std::cout << "The address of the queue is: " << addressStr << std::endl;
+    const std::string tempDirPath = "./.temp";
+    const std::string mailboxAddrFile = "mailboxAddress.bin";
+    const std::string pathToMailboxAddrFile =
+        tempDirPath + "/" + mailboxAddrFile;
+
+    if (!std::filesystem::exists(tempDirPath)) {
+        if (!std::filesystem::create_directory(tempDirPath)) {
+            std::cerr << "Could not create temp directory" << std::endl;
+            return -1;
+        }
+    }
+
+    std::ofstream addressFile(pathToMailboxAddrFile, std::ios::binary);
+    if(!addressFile) {
+        std::cerr << "Could not create binary file for mailbox address writing" << std::endl;
+        return -1;
+    }
+
+    ThreadSafeQueue* mailboxAddress = &messageQueue;
+    addressFile.write(reinterpret_cast<char*>(mailboxAddress), sizeof(mailboxAddress));
+    addressFile.close();
+
+    std::cout << "Setup done!" << std::endl;
+
+    std::cout << "Starting cleanup..." << std::endl;
+
+    if (!std::filesystem::exists(tempDirPath)) {
+        std::cerr << "Temp directory does not exist!" << std::endl;
+        return -1;
+    }
+
+    std::error_code ec;
+    std::filesystem::remove_all(tempDirPath, ec);
+
+    if(ec) {
+        std::cerr << "Error: " << ec.message() << std::endl;
+        return -1;
+    }
+
+    std::cout << "Cleanup done!" << std::endl;
 
     return 0;
 }
