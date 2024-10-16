83c6e75477a49dba999b658762467f98a07828d3
vovatrykoz <vovatrykoz@gmail.com>
2024-10-16 16:14:14 +0200

    minor changes

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "queue/ThreadSafeQueue.h"
+
 int main(void) {
+    ThreadSafeQueue messageQueue;
+
+    std::ostringstream oss;
+    oss << &messageQueue;
+    std::string addressStr = oss.str();
+    std::cout << "The address of the queue is: " << addressStr << std::endl;
+
     return 0;
 }
